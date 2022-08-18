9566d79e748932cfad1cb6a4056f2ea25743310a
andreilevin <51230917+andreilevin@users.noreply.github.com>
2022-08-18 13:19:17 -0400

    Update streamlit-app.py

--- streamlit-app.py
+++ streamlit-app.py
@@ -33,44 +33,11 @@ st.markdown('''
 st.write('---')
 
 
-st.write("a logo and text next to eachother")
-col1, mid, col2 = st.columns([1,1,20])
-with col1:
-    st.image('figures/heroguy.png', width=100)
-with col2:
-    st.write('A Name')
-  
-import base64
+st.sidebar.image('figures/heroguy.png', width=100)
 
-LOGO_IMAGE = 'figures/heroguy.png'
+st.sidebar.markdown(" # Select Player from Team:")
+team = st.sidebar.selectbox("Team:",
+                                   sorted(team_to_tm.keys()))
+tm = team_to_tm[team]
 
-st.markdown(
-    """
-    <style>
-    .container {
-        display: flex;
-    }
-    .logo-text {
-        font-weight:700 !important;
-        font-size:50px !important;
-        color: #f9a01b !important;
-        padding-top: 75px !important;
-    }
-    .logo-img {
-        float:right;
-    }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
-
-st.markdown(
-    f"""
-    <div class="container">
-        <img class="logo-img" src="data:image/png;base64,{base64.b64encode(open(LOGO_IMAGE, "rb").read()).decode()}">
-        <p class="logo-text">Logo Much ?</p>
-    </div>
-    """,
-    unsafe_allow_html=True
-)
 
